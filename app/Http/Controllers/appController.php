be61be3484089b4aedb2f4bc448e887a2c2ded7c
Jan Köster <jan.koester@bbaw.de>
2021-01-20 13:40:17 +0000

    js rerouting

--- app/Http/Controllers/appController.php
+++ app/Http/Controllers/appController.php
@@ -5,6 +5,7 @@ namespace App\Http\Controllers;
 use App\Http\Controllers\Controller;
 use DB;
 use Request;
+use Response;
 
 class appController extends Controller {
 
@@ -13,5 +14,12 @@ class appController extends Controller {
         return view ('layouts.app', ['app' => [
             'language'  => substr(Request::server('HTTP_ACCEPT_LANGUAGE'), 0, 2) === 'de' ? 'de' : 'en'
         ]]);
+    }
+
+    public function provideJS ($file) {
+        $contents = \File::get('/opt/projects/'.(is_dir('/opt/projects/cil/src') ? 'cil' : 'cil-laravel').'/src/public/js/'.$file);
+        $response = Response::make($contents, 200);
+        $response->header('Content-Type', 'application/javascript');
+        return $response;
     }
 }
